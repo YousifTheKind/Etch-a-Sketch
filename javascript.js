5c12314526e925966062dd115d4aeaa2478565a0
yousif <iikxt@yahoo.com>
2024-02-16 13:34:11 +0300

    Implement "pen" effect & fixed grid effect

--- javascript.js
+++ javascript.js
@@ -3,22 +3,78 @@
 // write for loop inside the function that takes the arguments and creates 16x16 divs
 
 const container = document.querySelector("#container");
+const newGrid = document.querySelector("#newGrid")
+const gridStyle = document.querySelectorAll('.gridStyle')
+newGrid.className = "buttonPos";
 
-function createGrid(rowsNum, columnsNum) {
-    for (let i = 0; i < rowsNum; i++) {
-        let newRow = document.createElement("div")
-        container.appendChild(newRow);
+
+function createGrid(gridSize) {
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let newRow = document.createElement("div");
+        let a = gridSize;
+        const boxSize = 600 / gridSize;
         newRow.className = "gridStyle";
-    for (let j = 1; j < columnsNum; j++) {
-        let newColumn = document.createElement("div");
-        container.appendChild(newColumn)
-        newColumn.className = "gridStyle";
-    }
-        
+        newRow.style.width = boxSize + 'px';
+        newRow.style.height = boxSize + 'px';        
+        container.appendChild(newRow);
      }
 }
 
-createGrid(16,16);
 
 
-console.log(container.children.length)
+
+// add an event listener that activates when the mouse hovers over a dive
+//change the backgroundcolor of the div when mouse hovers
+//
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target !== e.currentTarget)
+    {
+        e.stopPropagation()
+        e.target.style.backgroundColor = "red";    
+    }
+
+});
+
+
+// create a button in html with id
+// get a ref to the button in js
+// position the button in the top of the screen using css class
+// add event listener in js to call a function when the button is clicked
+// write new function
+    // prompt the user when the button is clicked
+    // prompt should ask the user to enter a number that represents the number of rows columns (doesn't matter)
+    // take the user input and use it as input to the createGrid function
+// figure out how to cram the squares in the same total space
+
+
+newGrid.addEventListener("click", () => {
+    let gridSize = parseInt(prompt("Please enter the new grid size. i.e for a 16x16 grid enter '16' (Max 100)"), 10);
+    
+    let boxes = document.querySelectorAll('.gridStyle');
+
+    boxes.forEach(box => {
+        box.remove();
+    })
+
+    while(isNaN(gridSize) || gridSize > 100 || gridSize < 1){
+        gridSize = parseInt(prompt("please enter a number between 1 & 100"), 10);
+    }
+
+    //createGrid(gridSize, gridSize);
+    createGrid(gridSize);
+
+});
+
+
+
+
+// in the newGrid event listener create a ref to all the divs
+// forEach box box.remove
+// createGrid(gridSize, gridSize)
+// in the createGrid function create new variable boxSize that takes a int(try 100) then devide it by rowNum
+// set width and height property of the div based on boxSize
+
+
+
